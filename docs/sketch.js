89c62761d9be25466371c417f4e46a578a91000b
breakthefunk <maciej.k.krupa@gmail.com>
2018-07-10 01:10:32 +0200

    9

--- docs/sketch.js
+++ docs/sketch.js
@@ -1,49 +1,12 @@
-/*var stars = new Array(100);
-
 function setup() {
-  createCanvas(400, 400); 
-  for (var i = 0; i < stars.lenght; i++) {
-    stars[i]= new StarObject();
-  }
-
+  createCanvas(640, 480);
 }
 
 function draw() {
-  background(0);
-  for (var i = 0; i < stars.lenght; i++) {
-    stars[i].update();
-    stars = console.log();
-    stars[i].show();
-  }
-    
-}
-
-class StarObject {
-  constructor() { 
-    var x;
-    var y;
-    var z;
-    this.StarObject();
-    this.show();
-  }
-  StarObject() {
-      this.x = random(0, width);
-      this.y = random(0, height);
-      this.z = random(0, width);
-  }
-  update() {
-  }
-  show() {
+  if (mouseIsPressed) {
+    fill(0);
+  } else {
     fill(255);
-    noStroke();
-    ellipse(x, y, 8, 8);  
   }
-}*/
-
-function setup() {
-
-}
-
-function draw() {
-  ellipse(50, 50, 80, 80);
+  ellipse(mouseX, mouseY, 80, 80);
 }
