544edf9c166c3054003d1bfc9d736b69c146b49b
breakthefunk <maciej.k.krupa@gmail.com>
2018-07-09 23:24:30 +0200

    5

--- docs/sketch.js
+++ docs/sketch.js
@@ -4,14 +4,16 @@ var StarArray = [];
 var stars = new StarArray(100);
 
 function setup() {
-  createCanvas(400, 400);
-  for (var i = 0; i < stars.lenght; i++)
+  createCanvas(400, 400); 
+  for (var i = 0; i < stars.lenght; i++) {
     var stars = new StarArray(100);
+  }
+
 }
 
 void draw() {
   background(0);
-  for (var i = 0; i < stars.lenght; i++)
+  for (var i = 0; i < stars.lenght; i++) {
     stars[i].update();
     stars[i].show();
   }
