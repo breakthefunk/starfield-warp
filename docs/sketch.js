07f1a926cb7c01d0d68146011b8f8cf776f157bd
breakthefunk <maciej.k.krupa@gmail.com>
2018-07-10 00:25:54 +0200

    9

--- docs/sketch.js
+++ docs/sketch.js
@@ -20,3 +20,23 @@ function draw() {
     
 }
 
+class StarObject {
+  console.log("connected");
+  constructor() { 
+  var x;
+  var y;
+  var z;
+  }
+  StarObject() {
+      this.x = random(0, width);
+      this.y = random(0, height);
+      this.z = random(0, width);
+  }
+  void update() {
+  }
+  void show() {
+      fill(255);
+      noStroke();
+      ellipse(x, y, 8, 8);
+  }
+}
